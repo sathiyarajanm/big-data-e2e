060c7f5dbd0452bdd353b3b3ac70890b312c0ce8
root <sathiyarajan.m@prodapt.com>
2018-10-07 21:51:51 +0530

    Added spark and open ssh configs

--- scripts/check-package.sh
+++ scripts/check-package.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
 if ! type "java" > /dev/null; then
-sudo add-apt-repository ppa:webupd8team/java
-sudo opt update; sudo apt install oracle-java8-installer
-sudo apt install oracle-java8-set-default
+	sudo add-apt-repository ppa:webupd8team/java
+	sudo opt update; sudo apt install oracle-java8-installer
+	sudo apt install oracle-java8-set-default
 else
-	  echo "Java has installed"
-  	fi
+	echo "Java has installed"
+fi
 
 if ! type "vim" > /dev/null; then
 
 	sudo apt-get install vim
 else
 	echo "vim has installed"
-	fi
+fi
 if ! type "subl" > /dev/null; then
-echo "installing sublime text"
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
+	echo "installing sublime text"
+	sudo add-apt-repository ppa:webupd8team/sublime-text-3
+	sudo apt-get update
+	sudo apt-get install sublime-text-installer
 else
 	echo "sublime-3 has installed"
-	fi
+fi
+
+if ! type "ssh" > /dev/null; then
+       echo "installing open ssh client and ssh server modules"
+	sudo apt-get install openssh-server openssh-client
+	ssh-keygen -t rsa -P ""
+	cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+else
+        echo "ssh client and ssh server are already installed"
+fi
+
 if ! type "expect" > /dev/null; then
 	echo "installing ssh pass and expect module"
 	sudo apt-get install ssh-pass
 	sudo apt-get install expect
 else
 	echo "ssh-pass and expect modules are already installed"
-	fi
+fi
 
-			
+		
